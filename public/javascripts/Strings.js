29c4d9d7ae42af0f8539673a40cb929cf9d772e9
itsjh <93984898+itsjh1242@users.noreply.github.com>
2023-07-09 21:48:05 +0900

    commit : landingPage
    create Profile, Menu Content of landingPage

--- public/javascripts/Strings.js
+++ public/javascripts/Strings.js
@@ -1,14 +1,33 @@
-function stringConvert(page) {
-  Object.entries(page).forEach(([key, value]) => {
-    document.getElementById(key).innerHTML = value;
-  });
-}
+// function stringConvert(page) {
+//   Object.entries(page).forEach(([key, value]) => {
+//     document.getElementById(key).innerHTML = value.toString();
+//   });
+// }
+
+// function IndexPageMenuConvert() {
+//   const MenuCount = Object.keys(IndexPage.menu).length;
+//   for (let i = 0; i < MenuCount; i++) {
+//     document.getElementById("menu_title_" + i).innerHTML = IndexPage.menu[i][0];
+//     document.getElementById("menu_content_" + i).innerHTML = IndexPage.menu[i][1];
+//   }
+// }
 
 const IndexPage = {
   // Landing Page
   profile_banner: "Personal Portfolio",
   profile_title: "Hi,\nI'm JunHyeon Kim",
-  left_text: "Profile",
-  right_text:
+  profile_left_text: "Profile",
+  profile_right_text:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  menu: {
+    0: ["Experiences", "Job Responsibilities, Achievements"],
+    1: ["Works", "Personal or Team Project Works"],
+    2: ["Skills & Certificates", "Key Skills, Certificates"],
+    3: ["Activities", "Extracurricular Activities, Leadership Roles"],
+  },
+  menu_readmore: "Read More",
+};
+
+module.exports = {
+  IndexPage: IndexPage,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const IndexPage = require("../public/javascripts/Strings").IndexPage;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const MenuCount = Object.keys(IndexPage.menu).length;
+
+  res.render("index", {
+    IndexPage: IndexPage,
+    MenuCount: MenuCount,
+  });
 });
 
 module.exports = router;
