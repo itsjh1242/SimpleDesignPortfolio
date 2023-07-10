4cbcc18148824dc50b0a718b78de8e323532b17f
itsjh <93984898+itsjh1242@users.noreply.github.com>
2023-07-10 17:37:47 +0900

    추가 : profile Image slide, footer

--- public/javascripts/Strings.js
+++ public/javascripts/Strings.js
@@ -13,19 +13,30 @@
 // }
 
 const IndexPage = {
-  // Landing Page
+  // Landing Page Profile banner
   profile_banner: "Personal Portfolio",
-  profile_title: "Hi,\nI'm JunHyeon Kim",
+  // Your Name
+  profile_title: "JunHyeon Kim",
+  // Profile About Left Text
   profile_left_text: "Profile",
+  // Profile About Right Text
   profile_right_text:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  // Portfolio Menu Contents
   menu: {
     0: ["Experiences", "Job Responsibilities, Achievements"],
     1: ["Works", "Personal or Team Project Works"],
     2: ["Skills & Certificates", "Key Skills, Certificates"],
     3: ["Activities", "Extracurricular Activities, Leadership Roles"],
   },
+  // Portfolio Menu Read More Text
   menu_readmore: "Read More",
+  profile_slide: {
+    0: "KIM JUN HYEON",
+    1: "Bachelor’s Degree in Software Enginnering",
+    2: "Dongseo University, Busan,South Korea",
+    3: "Mar.2018 – Feb.2024",
+  },
 };
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,17 @@ const IndexPage = require("../public/javascripts/Strings").IndexPage;
 /* GET home page. */
 router.get("/", function (req, res, next) {
   const MenuCount = Object.keys(IndexPage.menu).length;
+  const SlideTextCount = Object.keys(IndexPage.profile_slide).length;
 
   res.render("index", {
     IndexPage: IndexPage,
     MenuCount: MenuCount,
+    SlideTextCount: SlideTextCount,
   });
+});
+
+router.get("/experience", function (req, res, next) {
+  res.render("experience", {});
 });
 
 module.exports = router;
