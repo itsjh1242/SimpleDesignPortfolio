61038eaff325317b3509373763065eb57125d956
itsjh <93984898+itsjh1242@users.noreply.github.com>
2023-10-17 13:19:52 +0900

    Revert "Merge branch 'main' of https://github.com/itsjh1242/SimpleDesignPortfolio"
    This reverts commit 5d32022e15766c39af0da2f87bcf4eddb11fddb1, reversing
    changes made to 17b190dd7e456d0304ca3c3d3d65f898fb4632b4.

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var router = express.Router();
+const IndexPage = require("../public/javascripts/config").IndexPage;
+const ExperiencePage = require("../public/javascripts/config").ExperiencePage;
+const WorksPage = require("../public/javascripts/config").WorksPage;
+const SkillsPage = require("../public/javascripts/config").SkillsPage;
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const MenuCount = Object.keys(IndexPage.menu).length;
+  const SlideTextCount = Object.keys(IndexPage.profile_slide).length;
+
+  res.render("index", {
+    IndexPage: IndexPage,
+    MenuCount: MenuCount,
+    SlideTextCount: SlideTextCount,
+  });
+});
+
+router.get("/menu/:index", function (req, res, next) {
+  if (req.params.index == 0) {
+    return res.render("experience", {
+      ExperiencePage: ExperiencePage,
+      menuTitle: "Experiences",
+      menuSubTitle: "Job Responsibilities, Achievements",
+    });
+  } else if (req.params.index == 1) {
+    return res.render("works", {
+      WorksPage: WorksPage,
+      menuTitle: "Works",
+      menuSubTitle: "Personal or Team Project Works",
+    });
+  } else if (req.params.index == 2) {
+    return res.render("skills", {
+      SkillsPage: SkillsPage,
+      menuTitle: "Skills & Certificates",
+      menuSubTitle: "Key Skills, Certificates"
+    });
+  } else if (req.params.index == 3) {
+    return res.render("activities", {});
+  }
+});
+
+module.exports = router;
